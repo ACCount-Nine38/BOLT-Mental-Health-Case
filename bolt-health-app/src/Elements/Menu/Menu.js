739612a6fb322407c3e1c59d2ab0ff23677dced6
DiverseFX <rogamesfx@gmail.com>
2021-03-16 18:44:31 -0400

    Basic Side Nav

--- bolt-health-app/src/Elements/Menu/Menu.js
+++ bolt-health-app/src/Elements/Menu/Menu.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import Drawer from './Drawer/Drawer'
 import './Menu.css'
-import Backdrop from '../Backdrop/Backdrop'
 
-const Menu = (props) => {
-    let attachedClasses = ['Menu', 'Close'];
-    if (props.open) {
-        attachedClasses = ['Menu', 'Open']
+import MenuIcon from './menu.png'
+import AccountIcon from './account.png'
+
+function Menu() {
+    const [toggle, setToggle] = React.useState(false);
+    // changes toggle state to show or not show menu
+    const MenuToggle = () => {
+        setToggle(!toggle);
     }
-    return (
-        <>
-            <Backdrop show={props.open} clicked={props.closed} />
-            <div className={attachedClasses.join(' ')} onClick={props.closed}>
-                <Drawer />
-                <nav>
-                </nav>
-            </div>
-        </>
-    );
-};
 
-export default Menu;
+    return (
+        <div>
+            <img className={toggle ? "iconActive" : "icon"} onClick={MenuToggle} src={MenuIcon} alt="Menu icon"></img>
+            <div className={toggle ? "drawerActive" : "drawerInactive"}>
+                <ul>
+                    <li><a href="/missions">Missions</a></li>
+                    <li><a href="/missions">Enrolled</a></li>
+                    <li><a href="/missions">Connect</a></li>
+                    <li><a href="/missions">Premium</a></li>
+                    <li listStyleImage={AccountIcon}><a>Account</a></li>
+                    <li><a>Settings</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Menu

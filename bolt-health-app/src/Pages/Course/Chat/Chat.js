3fd647f7dd1e71a38c8dd3ba4bbce2f47de78e8c
DiverseFX <rogamesfx@gmail.com>
2021-03-18 20:05:43 -0400

    Course Main and Chat Finished

--- bolt-health-app/src/Pages/Course/Chat/Chat.js
+++ bolt-health-app/src/Pages/Course/Chat/Chat.js
@@ -1,6 +1,23 @@
+import {useState} from 'react'
+import './Chat.css'
+import * as BiIcons from 'react-icons/bi'
+
 function Chat () {
+    const [hasText, setHasText] = useState(false);
+    const newChat = () => {
+        setHasText(true);
+        var history = document.getElementById("chatField").value;
+        document.getElementById("chatHistory").innerHTML = history
+        document.getElementById("chatField").value = "";
+    }
     return (
         <> 
+        <div className='chatPage'>
+            <input type="text" id="chatField" name="chatField" className='chatField'></input>
+            <BiIcons.BiSend onClick={newChat} className='send' />
+            
+            <div className={hasText ? 'chatBox' : null} id="chatHistory"></div>
+        </div>
         </>
         
     );
--- bolt-health-app/src/Pages/Course/Main/Course.js
+++ bolt-health-app/src/Pages/Course/Main/Course.js
@@ -1,6 +1,16 @@
+import {Link} from 'react-router-dom'
+import './Course.css'
+import '../../../Elements/Navbar/Navbar.css'
+
+import * as BsIcons from 'react-icons/bs'
+
 function Course () {
     return (
         <> 
+            <div className='coursePage'></div>
+            <Link to='/course/chat'>
+                <BsIcons.BsFillChatFill className='icons chatIcon' />
+            </Link>
         </>
         
     );

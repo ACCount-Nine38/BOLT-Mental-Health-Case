ab92dc841b9310b69ba05e57fa66134c9acc83ef
DiverseFX <rogamesfx@gmail.com>
2021-03-18 20:24:46 -0400

    Login screen complete

--- bolt-health-app/src/Pages/Login/Login.js
+++ bolt-health-app/src/Pages/Login/Login.js
@@ -3,7 +3,15 @@ import './Login.css'
 
 function Login () {
     return (
-        <a href='missions' className='ButtonPrimary login'>Login</a>
+        <>
+            <div className='loginBack'>
+                <div className='fakeFieldBox'>  
+                    <input type="username" placeholder="Username" className='fakeInputs' /> <br />
+                    <input type="password" placeholder="Password" className='fakeInputs' />
+                </div>
+                <a href='missions' className='ButtonPrimary login'>Login</a>
+            </div>
+        </>
     );
 };
 
